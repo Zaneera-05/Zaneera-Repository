949d96d972500f8092214945a9c939aa805adad5
Zaneera-05 <zaneerabintezahid@gmail.com>
2024-12-04 10:35:17 +0500

    Add files via upload

---
+++ PF Lab 12 Task 1(b).cpp
@@ -0,0 +1,13 @@
+#include<iostream>
+#include <cstring>
+using namespace std;
+int main()
+{
+	string str1 = "Hello, ";
+	string str2 = "World!";
+	
+    string result = str1.append(str2);
+	
+	cout<<"Concatenated string: "<<result;
+	return 0;
+}
---
+++ PF Lab 12 Task 2.cpp
@@ -0,0 +1,12 @@
+#include<iostream>
+using namespace std;
+#include<cstring>
+int main()
+{
+	string str="Hello, World!";
+	
+	int length = str.length();
+	
+	cout<<"The length of the string is: "<<length<<endl;
+	return 0;
+}
